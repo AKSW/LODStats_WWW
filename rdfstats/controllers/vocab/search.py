f409552ef7854b2708da3e7890473ddcf2452324
Ivan Ermilov <iermilov@informatik.uni-leipzig.de>
2014-05-06 17:18:54 +0200

    vocabulary search by property

--- rdfstats/controllers/vocab/search.py
+++ rdfstats/controllers/vocab/search.py
@@ -12,7 +12,7 @@ class SearchController(BaseController):
     """REST Controller styled on the Atom Publishing Protocol"""
     # To properly map this controller, ensure your config/routing.py
     # file has a resource setup:
-    #     map.resource('search', 'search', controller='vocabulary/search', 
+    #     map.resource('search', 'search', controller='vocabulary/search',
     #         path_prefix='/vocabulary', name_prefix='vocabulary_')
 
     def index(self, format='html'):
@@ -48,24 +48,27 @@ class SearchController(BaseController):
     def show(self, id, format='html'):
         """GET /vocabulary/search/id: Show a specific item"""
         # url('vocabulary_search', id=ID)
-        query = """
-            SELECT DISTINCT rdfdoc.id, rdfdoc.uri 
-            FROM rdfdoc, stat_result, rdf_property_stat, rdf_property 
-            WHERE rdf_property.uri=:uri AND
-            rdf_property.id=rdf_property_stat.rdf_property_id AND
-            rdf_property_stat.stat_result_id=stat_result.id AND
-            stat_result.rdfdoc_id=rdfdoc.id 
-            ORDER BY rdfdoc.id;
-        """
         import re
         id = re.sub("http:/", "http://", id)
+        query = """
+            SELECT DISTINCT rdfdoc.id, rdfdoc.uri
+            FROM rdfdoc, stat_result, rdf_property_stat, rdf_property
+            WHERE rdf_property.uri='%s' AND
+            rdf_property.id=rdf_property_stat.rdf_property_id AND
+            rdf_property_stat.stat_result_id=stat_result.id AND
+            stat_result.rdfdoc_id=rdfdoc.id
+            ORDER BY rdfdoc.id;
+        """ % (id)
         try:
-            dataset = Session.execute(query, {'uri': id})
-            #output_url = h.url(controller="rdfdocs", action="show", id=dataset.id, qualified=True)
-            return json.dumps(dataset)
+            datasets = []
+            result = Session.execute(query)
+            for row in result:
+                obj = {"id": row[0],
+                    "uri": row[1]}
+                datasets.append(obj)
+            return json.dumps(datasets)
         except:
             return json.dumps('')
-        return ""
 
     def edit(self, id, format='html'):
         """GET /vocabulary/search/id/edit: Form to edit an existing item"""
