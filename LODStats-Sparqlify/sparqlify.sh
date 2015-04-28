f94ee5d8d918bb6ecf477eb697a3eef58552da48
Ivan Ermilov <earthquakesan@gmail.com>
2015-04-28 19:44:57 +0200

    sparqlify script final version

--- LODStats-Sparqlify/sparqlify.sh
+++ LODStats-Sparqlify/sparqlify.sh
@@ -1,9 +1,9 @@
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdfdoc.sml -D > ./datasets/rdfdoc.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/ckan_catalog.sml -D > ./datasets/ckan_catalog.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/stat_result.sml -D > ./datasets/stat_result.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_class.sml -D > ./datasets/rdf_class.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_datatype.sml -D > ./datasets/rdf_datatype.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_property.sml -D > ./datasets/rdf_property.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/defined_class.sml -D > ./datasets/defined_class.nt
-#sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/language.sml -D > ./datasets/language.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdfdoc.sml -D > ./datasets/rdfdoc.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/ckan_catalog.sml -D > ./datasets/ckan_catalog.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/stat_result.sml -D > ./datasets/stat_result.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_class.sml -D > ./datasets/rdf_class.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_datatype.sml -D > ./datasets/rdf_datatype.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/rdf_property.sml -D > ./datasets/rdf_property.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/defined_class.sml -D > ./datasets/defined_class.nt
+sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/language.sml -D > ./datasets/language.nt
 sparqlify -h localhost -U postgres -W postgres -d lodstats -m ./mappings/vocab.sml -D > ./datasets/vocab.nt
